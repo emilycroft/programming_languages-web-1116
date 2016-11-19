1afe851985cff47fa483bdb7dca86c57d724b5d1
Emily Croft <emilyhcroft@gmail.com>
2016-11-19 17:36:26 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, language|
+    language.each do |language2, type_name|
+      type_name.each do |type_name2, value|
+        new_hash[language2] = { type_name2 => value, :style => [type] }
+      end
+    end
+  end
+  new_hash[:javascript][:style] << :oo
+  new_hash
 end
